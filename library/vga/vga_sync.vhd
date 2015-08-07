a6565a1c0cbd9d82522ebc44c7fca48046fa7fc7
Fabien André <fabien.andre@xion345.info>
2015-08-08 01:38:04 +0200

    Make VGA Synchronisation component generic

--- library/vga/vga_sync.vhd
+++ library/vga/vga_sync.vhd
@@ -6,6 +6,23 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity vga_sync is
+    generic(
+        CLOCK_DIVISOR: integer := 4;
+        --
+        SCREEN_WIDTH: integer := 640;
+        SCREEN_HEIGHT: integer := 480;
+        --
+        TOTAL_WIDTH: integer := 800;
+        TOTAL_HEIGHT: integer := 525;
+        --
+        HSYNC_START: integer := 656;
+        HSYNC_STOP: integer := 752;
+        HSYNC_POLARITY: std_logic := '0'; -- 0 negative polarity, 1 positive polarity
+        --
+        VSYNC_START: integer := 490;
+        VSYNC_STOP: integer := 492;
+        VSYNC_POLARITY: std_logic := '0' -- 0 negative polarity, 1 positive polarity
+    );
     port(
         clk, reset: in std_logic;
         hsync, vsync: out std_logic;
@@ -16,7 +33,6 @@ entity vga_sync is
 end vga_sync;
 
 architecture arch of vga_sync is
-    constant CLOCK_DIVISOR: integer := 4;
     constant CLOCK_DIVISOR_WIDTH: integer := 2;
     signal pixel_tick_internal: std_logic;
     -- Sync. counters
@@ -54,9 +70,9 @@ begin
         vcount_next <= vcount_reg;
 
         if pixel_tick_internal = '1' then
-            if hcount_reg = 799 then
+            if hcount_reg = TOTAL_WIDTH-1 then
                 hcount_next <= (others => '0');
-                if vcount_reg = 524 then
+                if vcount_reg = TOTAL_HEIGHT-1 then
                     vcount_next <= (others => '0');
                 else
                     vcount_next <= vcount_reg + 1;
@@ -68,10 +84,10 @@ begin
     end process;
 
     -- Hsync/Vsync
-    hsync_next <= '1' when hcount_reg >= 656 and hcount_reg < 752 else
-                  '0';
-    vsync_next <= '1' when vcount_reg >= 490 and vcount_reg < 491 else
-                  '0';
+    hsync_next <= HSYNC_POLARITY when hcount_reg >= HSYNC_START and hcount_reg < HSYNC_STOP-1 else
+                  not HSYNC_POLARITY;
+    vsync_next <= VSYNC_POLARITY when vcount_reg >= VSYNC_START and vcount_reg < VSYNC_STOP-1 else
+                  not VSYNC_POLARITY;
 
     -- Output
     pixel_tick <= pixel_tick_internal;
@@ -79,6 +95,6 @@ begin
     vsync <= vsync_reg;
     x <= std_logic_vector(hcount_reg);
     y <= std_logic_vector(vcount_reg);
-    video_on <= '1' when hcount_reg < 640 and vcount_reg < 480 else '0';
+    video_on <= '1' when hcount_reg < SCREEN_WIDTH and vcount_reg < SCREEN_HEIGHT else '0';
 
 end arch;
